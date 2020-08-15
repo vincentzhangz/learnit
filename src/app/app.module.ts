b80aac1bc93b57abf9c28e0e7983f0b92a6a20ec
Vincent <vincentzhangz@outlook.com>
2020-08-15 16:18:00 +0700

    reorganize module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {RegisterComponent} from './pages/register/register.component';
 import {FileUploadModule} from 'ng2-file-upload';
 import {MatBadgeModule} from '@angular/material/badge';
+import {CourseComponent} from './pages/course/course.component';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import {MatBadgeModule} from '@angular/material/badge';
     ProfileComponent,
     ForumDetailComponent,
     CourseDetailComponent,
-    RegisterComponent
+    RegisterComponent,
+    CourseComponent
 
   ],
   imports: [
