7a9f4514013769b4f1375df2f6f98365ab0bbfcd
Chandra <chandratan030501@gmail.com>
2020-08-15 11:38:44 +0700

    create file service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../models/user';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import {UploadFileService} from '../../services/upload-file.service'; 
+import {FileUploader} from 'ng2-file-upload';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
   user:User;
+  
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+      private fileService: UploadFileService
+    ) { }
   
   // loginForm = new FormGroup({
   //   name: new FormControl(""),
@@ -20,10 +24,17 @@ export class LoginComponent implements OnInit {
   //   role: new FormControl(""),
     
   // })
+
+  // for file upload example
+  // uploader:FileUploader = this.fileService.createUploader("hehe");  
+
+
+
   loginForm = this.fb.group({
     email:['',  Validators.required],
     password:['',  Validators.required],
   })
+
 
   ngOnInit(): void {
   }
---
+++ src/app/services/upload-file.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+declare var URL:string;
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFileService {
+
+  constructor() {
+ 
+  }
+  createUploader(url:string):FileUploader{
+    URL = url;
+    let uploader:FileUploader = new FileUploader({url:URL});
+    uploader.onCompleteItem = (file: any, response: any, status: any) => {
+      console.log('uploadedFile', file, status, response); // for checking purpose
+      alert('File upload success!!');
+    }
+    return uploader
+  }
+
+}
