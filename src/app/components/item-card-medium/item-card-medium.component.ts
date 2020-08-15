1baad62a433f63973261bff8c72850342da26766
Vincent <vincentzhangz@outlook.com>
2020-08-16 00:08:07 +0700

    add category UI on home

--- src/app/components/item-card-medium/item-card-medium.component.ts
+++ src/app/components/item-card-medium/item-card-medium.component.ts
@@ -1,23 +1,17 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-item-card-medium',
-  templateUrl: './item-card-medium.component.html',
-  styleUrls: ['./item-card-medium.component.sass']
+    selector: 'app-item-card-medium',
+    templateUrl: './item-card-medium.component.html',
+    styleUrls: ['./item-card-medium.component.sass']
 })
 export class ItemCardMediumComponent implements OnInit {
-  @Input() course: any;
+    @Input() course: any;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-    console.log(this.course);
-    // this.course = {
-    //   image: '../../../assets/img/lighting.jpg',
-    //   course_title: 'Test Title',
-    //   description: 'Test Desc'
-    // };
-  }
+    ngOnInit(): void {
+    }
 
 }
