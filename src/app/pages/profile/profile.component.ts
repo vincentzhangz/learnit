a8e9133bc6b751adf458f4016bcb10f3e0f59e3f
Chandra <chandratan030501@gmail.com>
2020-08-15 14:01:30 +0700

    profile-page done

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { UploadFileService } from 'src/app/services/upload-file.service';
+import {FileUploader} from "ng2-file-upload";
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,10 +9,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() {
+  constructor(private fb: FormBuilder,
+      private fileService: UploadFileService
+    ) {
   }
+  updateForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    gender: ['', Validators.required],
+    role: ['', Validators.required],
+  });
+
+  uploader:FileUploader = this.fileService.createUploader("localhost:4200/api/image/userimage");
 
   ngOnInit(): void {
+  }
+
+  test(){
+
   }
 
 }
